d1a037dc9b6da87f937cb362360fa35819157339
akshata_rn <akshatarn123@gamail.com>
2023-01-08 18:36:36 +0530

    Dice map and finding max min

--- Datastructures.js
+++ Datastructures.js
@@ -1,10 +1,88 @@
-let arr = new Array();
-let i,n=100;
-for(i=1;i<n;i++)
+let map = new Map();
+let maxCount=10;
+let oneCount=0,twoCount=0,threeCount=0,fourCount=0,fiveCount=0,sixCount=0;
+bool = true;
+while(bool)
 {
-    if(i%11==0)
+    let rand = Math.floor(Math.random()*6+1);
+    switch(rand)
     {
-        arr.push(i);
+        case 1:
+            if(oneCount == maxCount)
+            {
+                bool = false;
+            }
+            else
+            {
+                oneCount++;
+            }
+            break;
+        case 2:
+            if(twoCount == maxCount)
+            {
+                bool = false;
+            }
+            else
+            {
+                twoCount++;
+            }
+            break;
+        case 3:
+            if(threeCount == maxCount)
+            {
+                bool = false;
+            }
+            else
+            {
+                threeCount++;
+            }
+            break;
+        case 4:
+            if(fourCount == maxCount)
+            {
+                bool = false;
+            }
+            else
+            {
+                fourCount++;
+            }
+            break;
+        case 5:
+            if(fiveCount == maxCount)
+            {
+                bool = false;
+            }
+            else
+            {
+                fiveCount++;
+            }
+            break;
+        case 6:
+            if(sixCount == maxCount)
+            {
+                bool = false;
+            }
+            else
+            {
+                sixCount++;
+            }
+            break;      
+            
     }
 }
-console.log("Repeated numbers are:"+arr);
+map.set(1,oneCount);
+map.set(2,twoCount);
+map.set(3,threeCount);
+map.set(4,fourCount);
+map.set(5,fiveCount);
+map.set(6,sixCount);
+console.log(map)
+
+let minTimes = Math.min(...map.values());
+let maxTimes = Math.max(...map.values());
+function getKey(values)
+{
+    return[...map].find(([key,val])=>val==value)
+}
+console.log("Maximum Times is :"+getKey(maxTimes));
+console.log("Minimum Times is :"+getKey(minTimes));
